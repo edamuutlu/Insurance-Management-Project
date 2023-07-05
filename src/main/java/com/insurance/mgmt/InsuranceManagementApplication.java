7578ad6dde59136eca64128de4e8842ec434248c
Eda Nur Mutlu <112180102+edamuutlu@users.noreply.github.com>
2023-07-05 09:58:21 +0300

    Insurance-Management-Project

---
+++ src/main/java/com/insurance/mgmt/InsuranceManagementApplication.java
@@ -0,0 +1,13 @@
+package com.insurance.mgmt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsuranceManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InsuranceManagementApplication.class, args);
+	}
+
+}
