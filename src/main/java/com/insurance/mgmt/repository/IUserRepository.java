b37488eb6bcbff4d6e781e8d5bb59c0e76a59541
Eda Nur Mutlu <112180102+edamuutlu@users.noreply.github.com>
2023-07-18 15:41:34 +0300

    unused methods deleted

--- src/main/java/com/insurance/mgmt/repository/IUserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.insurance.mgmt.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.insurance.mgmt.entity.User;
-
-public interface IUserRepository extends JpaRepository<User,Integer>{ //Object, id (type)
-
-}
