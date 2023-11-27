ed0e6fc57c21861e8dff76b7cf0bb14a915667dd
Eda Nur Mutlu <112180102+edamuutlu@users.noreply.github.com>
2023-11-27 15:18:17 +0300

    Trying @Query

--- src/main/java/com/insurance/mgmt/repository/ICustomerRepository.java
+++ src/main/java/com/insurance/mgmt/repository/ICustomerRepository.java
@@ -3,6 +3,7 @@ package com.insurance.mgmt.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.insurance.mgmt.entity.Customer;
@@ -10,8 +11,10 @@ import com.insurance.mgmt.entity.Customer;
 @Repository
 public interface ICustomerRepository extends JpaRepository<Customer,Integer>{ 
 	
+	@Query("FROM Customer WHERE status=?1")
 	List<Customer> findByStatus(int status);
 	
+	@Query("FROM Customer WHERE status=?1 and tc=?2")
 	List<Customer> findByStatusAndTc(int status, String tc);
 	
 	List<Customer> findByStatusAndEmail(int status, String email);
